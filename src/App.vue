42a00256952a38742d07dea32d639047ae2160b0
Aniket Aryamane <aryamane.aniket@rediffmail.com>
2021-12-24 12:43:14 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <simple-component :fname="name" :mname="middlename" />
+    <simple-component
+      :fname="surname"
+      :mname="middlename"
+      @clickFromChildComponent="handleClickInParent"
+    />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import SimpleComponent from "./components/SimpleComponent.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+    SimpleComponent,
+  },
+  methods: {
+    handleClickInParent(val1, val2) {
+      alert(val1 + " " + val2);
+    },
+  },
+  data() {
+    return {
+      name: "Aniket",
+      surname: "Aryamane",
+      middlename: "Arun",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SimpleComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <p @click="handleClick">
+    All of your components {{ fname }} {{ mname }} html goes inside these
+    template tags
+  </p>
+</template>
+
+<script>
+// All of your components JS code lives within this section
+// we can still do things such as `import lib from 'lib'` should we wish
+export default {
+  name: "simple-component",
+  data() {
+    return {
+      msg: "My Profile Component",
+    };
+  },
+  props: ["fname", "mname"],
+  methods: {
+    handleClick() {
+      this.$emit("clickFromChildComponent", this.fname, this.mname);
+    },
+  },
+};
+</script>
+
+<style scoped>
+p {
+  background-color: orange;
+}
+</style>
